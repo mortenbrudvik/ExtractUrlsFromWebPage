5c93e796f4bb9e9849ee3e3e9c092db5163eb437
Morten Brudvik <morten@brudvik.com>
2023-05-20 09:27:42 +0200

    chore: Fix formatting issue

--- src/ExtractUrls/LinkExtractor.cs
+++ src/ExtractUrls/LinkExtractor.cs
@@ -38,7 +38,7 @@ public static class LinkExtractor
             })
             .ToList();
     }
-    
+
     public static async Task WriteLinksToFileAsync(List<Link> links, string fileName)
     {
         await using var writer = new StreamWriter(fileName);
@@ -46,7 +46,7 @@ public static class LinkExtractor
         var tasks = links.Select(link => ProcessLinkAsync(link, writer));
         await Task.WhenAll(tasks);
     }
-    
+
     private static async Task ProcessLinkAsync(Link link, StreamWriter writer)
     {
         Console.WriteLine($"Processing '{link.Title}' {link.Href}");
@@ -56,7 +56,7 @@ public static class LinkExtractor
             var linkDoc = await GetHtmlDocumentAsync(link.Href);
             var title = link.Title ?? GetTitle(linkDoc).IfNone(link.Href);
             var description = GetDescription(linkDoc);
-            
+
             await WriteLinkToFileAsync(link.Href, title, description, writer);
         }
         catch (Exception e)
@@ -65,24 +65,24 @@ public static class LinkExtractor
             await WriteErrorToFileAsync(link.Href, title, e.Message, writer);
         }
     }
-    
+
     private static async Task<HtmlDocument> GetHtmlDocumentAsync(string link)
     {
-        var linkRequest = (HttpWebRequest)WebRequest.Create(link);
+        var linkRequest = (HttpWebRequest) WebRequest.Create(link);
         linkRequest.UserAgent =
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 Edge/16.16299";
-            
-        using var linkResponse = (HttpWebResponse)await linkRequest.GetResponseAsync();
+
+        using var linkResponse = (HttpWebResponse) await linkRequest.GetResponseAsync();
         using var linkStream = linkResponse.GetResponseStream();
         var linkDoc = new HtmlDocument();
         linkDoc.Load(linkStream);
         return linkDoc;
     }
-    
+
     private static Option<string> GetTitle(HtmlDocument linkDoc)
     {
         var tags = new[] {"title", "h1", "h2", "h3", "h4", "h5", "h6"};
-        
+
         foreach (var tag in tags)
         {
             var node = linkDoc.DocumentNode.Descendants(tag).FirstOrDefault();
@@ -90,43 +90,42 @@ public static class LinkExtractor
             if (node != null && !string.IsNullOrWhiteSpace(node.InnerText))
                 return TrimTitle(node.InnerText);
         }
+
         return None;
     }
-    
-
-private static string? GetDescription(HtmlDocument linkDoc) =>
-    linkDoc.DocumentNode.Descendants("meta")
-        .Where(m => m.GetAttributeValue("name", "") == "description")
-        .Select(m => m.GetAttributeValue("content", ""))
-        .FirstOrDefault();
 
 
-private static async Task WriteLinkToFileAsync(string url, string title, string? description, StreamWriter writer)
-{
-    await Console.Out.WriteLineAsync($"\nCompleted: '{title}' {url} {description ?? ""}");
-    var markdownLink = $"[{title}]({url})";
-    await writer.WriteLineAsync(markdownLink);
-            
-    if (!string.IsNullOrWhiteSpace(description))
-        await writer.WriteLineAsync(description);
-
-    await writer.WriteLineAsync();
-}
-
-private static async Task WriteErrorToFileAsync(string url, string title, string errorMessage, StreamWriter writer)
-{
-    await Console.Out.WriteLineAsync($"\nCompleted with error: '{title}' {url} \nError msg: {errorMessage}");
-
-    var markdownLink = $"[{title}]({url})";
-
-    await writer.WriteLineAsync(markdownLink);
-    await writer.WriteLineAsync("Error: " + errorMessage);
-    await writer.WriteLineAsync();
-}
+    private static string? GetDescription(HtmlDocument linkDoc) =>
+        linkDoc.DocumentNode.Descendants("meta")
+            .Where(m => m.GetAttributeValue("name", "") == "description")
+            .Select(m => m.GetAttributeValue("content", ""))
+            .FirstOrDefault();
 
 
+    private static async Task WriteLinkToFileAsync(string url, string title, string? description, StreamWriter writer)
+    {
+        await Console.Out.WriteLineAsync($"\nCompleted: '{title}' {url} {description ?? ""}");
+        var markdownLink = $"[{title}]({url})";
+        await writer.WriteLineAsync(markdownLink);
 
-    
+        if (!string.IsNullOrWhiteSpace(description))
+            await writer.WriteLineAsync(description);
+
+        await writer.WriteLineAsync();
+    }
+
+    private static async Task WriteErrorToFileAsync(string url, string title, string errorMessage, StreamWriter writer)
+    {
+        await Console.Out.WriteLineAsync($"\nCompleted with error: '{title}' {url} \nError msg: {errorMessage}");
+
+        var markdownLink = $"[{title}]({url})";
+
+        await writer.WriteLineAsync(markdownLink);
+        await writer.WriteLineAsync("Error: " + errorMessage);
+        await writer.WriteLineAsync();
+    }
+
+
     private static bool IsValidLink(string url, Uri rootUri) =>
         !string.IsNullOrEmpty(url) &&
         url.StartsWith("http") &&
